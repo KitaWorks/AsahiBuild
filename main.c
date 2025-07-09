f2fe6ca1f345ea44f9ce16102e7f81ed2ba8fdf8
Julian <JulianDroske@users.noreply.github.com>
2025-07-10 01:11:43 +0800

    main: refactor arg parsing logic
    Introduces functions 'print_help()', 'do_', a macro 'NEXT_OPTION' for a better
    arg parsing and project structure.

--- main.c
+++ main.c
@@ -3,33 +3,104 @@
 
 #include "version.h"
 
-int main(int argc, char *argv[]) {
-  char *filename = argv[1];
 
-  if (!argv[1]) {
-    printf("AsahiBuild Version %s\n", VERSION);
-    printf(" %s <action>\n", argv[0]);
-    printf("Usage:\n");
-    printf("new\t-\tCreate project\n");
-    printf("build\t-\tBuild project\n");
-    printf("config\t-\tConfigure project options\n");
+/* ACTIONS */
+
+int do_new_project(const char *name) {
+  // TODO: new project
+  return 0;
+}
+
+int do_build() {
+  // TODO: build project
+  return 0;
+}
+
+int do_get_config(const char *key) {
+  // TODO: get config
+  return 0;
+}
+
+int do_set_config(const char *key, const char *value) {
+  // TODO: set config
+  return 0;
+}
+
+
+/* MAIN PROCEDURE */
+
+void print_help(const char *program_name) {
+  printf("AsahiBuild version %s\n", VERSION);
+  printf("Usage: %s <action> ...\n", program_name);
+  printf("action:\n%s",
+    "  new <name>\n"
+    "    Create a project using name <name>\n"
+    "  build\n"
+    "    Build current project\n"
+    "  config get <key>\n"
+    "    Get project option\n"
+    "  config set <key> <value>\n"
+    "    Set project option\n"
+  );
+}
+
+
+#define NEXT_OPTION(argc, argv) (argc > 0? (--argc, ++argv, *(argv - 1)): NULL)
+
+int main(int argc, char *argv[]) {
+  const char *program_name = NEXT_OPTION(argc, argv);
+
+  const char *action = NEXT_OPTION(argc, argv);
+  if (!action) {
+    puts("An action is needed.");
+    print_help(program_name);
     return 1;
   }
 
-  if (strcmp(argv[1], "new") == 0) {
-    if (argc < 3) {
-      printf("Project name required.\n");
+  if (strcmp(action, "new") == 0) {
+    const char *project_name = NEXT_OPTION(argc, argv);
+    if (!project_name) {
+      puts("A project name is required.");
+      print_help(program_name);
       return 1;
     }
-    char *project_name = argv[2];
-    // TODO: new project
-  } else if (strcmp(argv[1], "build") == 0) {
-    // TODO: build project
-  } else if (strcmp(argv[1], "config") == 0) {
-    // TODO: config
+    return do_new_project(project_name);
+  } else if (strcmp(action, "build") == 0) {
+    return do_build();
+  } else if (strcmp(action, "config") == 0) {
+    const char *config_action = NEXT_OPTION(argc, argv);
+    if (!config_action) {
+      puts("A config operation is required.");
+      print_help(program_name);
+      return 1;
+    }
+    if (strcmp(config_action, "get") == 0) {
+      const char *key = NEXT_OPTION(argc, argv);
+      if (!key) {
+        puts("A key is required.");
+        print_help(program_name);
+        return 1;
+      }
+      return do_get_config(key);
+    } else if (strcmp(config_action, "set") == 0) {
+      const char *key = NEXT_OPTION(argc, argv);
+      const char *value = NEXT_OPTION(argc, argv);
+      if (!key || !value) {
+        puts("A key with a value is required.");
+        print_help(program_name);
+        return 1;
+      }
+      return do_set_config(key, value);
+    } else {
+      puts("Invalid config operation.");
+      print_help(program_name);
+      return 1;
+    }
   } else {
-    printf("Action not exist.\n");
+    puts("Unknown action.");
+    print_help(program_name);
   }
 
-  return 0;
+  return 1;
 }
+
