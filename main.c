8a6aea94be26e33f5f4223423f770bbc84be2feb
MinimalMio <yukati2024@outlook.com>
2025-07-09 15:40:54 +0800

    feat: at create project, more secure access for argv

--- main.c
+++ main.c
@@ -4,26 +4,30 @@
 #include "version.h"
 
 int main(int argc, char *argv[]) {
-    char *filename = argv[1];
+  char *filename = argv[1];
 
-    if (!argv[1]) {
-        printf("AsahiBuild Version %s\n", VERSION);
-        printf(" %s <action>\n", argv[0]);
-        printf("Usage:\n");
-        printf("new\t-\tCreate project\n");
-        printf("build\t-\tBuild project\n");
-        printf("config\t-\tConfigure project options\n");
-        return 1;
+  if (!argv[1]) {
+    printf("AsahiBuild Version %s\n", VERSION);
+    printf(" %s <action>\n", argv[0]);
+    printf("Usage:\n");
+    printf("new\t-\tCreate project\n");
+    printf("build\t-\tBuild project\n");
+    printf("config\t-\tConfigure project options\n");
+    return 1;
+  }
+
+  if (strcmp(argv[1], "new") == 0) {
+    if (argc < 3) {
+      printf("Project name required.\n");
+      return 1;
     }
+    char *project_name = argv[2];
+    // TODO: new project
+  } else if (strcmp(argv[1], "build") == 0) {
+    // TODO: build project
+  } else if (strcmp(argv[1], "config") == 0) {
+    // TODO: config
+  }
 
-    if (strcmp(argv[1], "new") == 0) {
-        char *project_name = argv[2];
-        // TODO: new project
-    } else if (strcmp(argv[1], "build") == 0) {
-        // TODO: build project
-    } else if (strcmp(argv[1], "config") == 0) {
-        // TODO: config
-    }
-
-    return 0;
+  return 0;
 }
